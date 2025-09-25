797d58578971b0726c81f0a984c8d202a5cc4d56
Andrii Butenko <andrejbutik@gmail.com>
2025-09-25 23:05:31 +0200

    Finished

--- src/server.js
+++ src/server.js
@@ -3,9 +3,7 @@ import express from 'express';
 import cors from 'cors';
 import pino from 'pino-http';
 const app = express();
-
 const PORT = process.env.PORT ?? 3030;
-
 app.use(express.json());
 app.use(cors());
 app.use(
@@ -17,7 +15,8 @@ app.use(
         colorize: true,
         translateTime: 'HH:MM:ss',
         ignore: 'pid,hostname',
-        messageFormat: '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
+        messageFormat:
+          '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
         hideObject: true,
       },
     },
@@ -33,20 +32,14 @@ app.get('/notes/:noteId', (req, res) => {
   const { noteId } = req.params;
   res.status(200).json({ message: `Retrieved note with ID: ${noteId}` });
 });
-
 app.use((req, res) => {
   res.status(404).json({ message: 'Route not found' });
 });
-
 app.use((err, req, res, next) => {
-  console.error('Error:', err.message);
   res.status(500).json({
-    message: 'Internal Server Error',
     error: err.message,
   });
 });
-
-// Запуск сервера
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
